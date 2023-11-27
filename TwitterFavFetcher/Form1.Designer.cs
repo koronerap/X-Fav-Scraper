7a5fc2759ffe435be2c7802a26c2ff6ac529efe2
Ahmet Erdoğan Akkulak <koronerofficial@gmail.com>
2023-11-27 14:33:28 +0300

    dev-time-1

---
+++ TwitterFavFetcher/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace TwitterFavFetcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.goButton = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.startButton = new System.Windows.Forms.Button();
+            this.postUrlTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.scrollNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.headerFillPanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scrollNumericUpDown)).BeginInit();
+            this.headerPanel.SuspendLayout();
+            this.headerFillPanel.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // goButton
+            // 
+            this.goButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.goButton.FlatAppearance.BorderSize = 0;
+            this.goButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goButton.ForeColor = System.Drawing.Color.White;
+            this.goButton.Location = new System.Drawing.Point(942, 5);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(100, 30);
+            this.goButton.TabIndex = 2;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = false;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(10, 10);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(849, 722);
+            this.webView21.TabIndex = 3;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Lime;
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(1048, 5);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 30);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // postUrlTextBox
+            // 
+            this.postUrlTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.postUrlTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.postUrlTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.postUrlTextBox.ForeColor = System.Drawing.Color.White;
+            this.postUrlTextBox.Location = new System.Drawing.Point(5, 6);
+            this.postUrlTextBox.Name = "postUrlTextBox";
+            this.postUrlTextBox.Size = new System.Drawing.Size(923, 19);
+            this.postUrlTextBox.TabIndex = 1;
+            this.postUrlTextBox.Text = "https://twitter.com/koronerap/status/1728398764476625221/likes";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.Location = new System.Drawing.Point(10, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 722);
+            this.listBox1.TabIndex = 6;
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.saveButton.FlatAppearance.BorderSize = 0;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.ForeColor = System.Drawing.Color.White;
+            this.saveButton.Location = new System.Drawing.Point(1048, 11);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 30);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.ForeColor = System.Drawing.Color.White;
+            this.countLabel.Location = new System.Drawing.Point(876, 20);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(47, 13);
+            this.countLabel.TabIndex = 7;
+            this.countLabel.Text = "Count: 0";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.ForeColor = System.Drawing.Color.White;
+            this.statusLabel.Location = new System.Drawing.Point(12, 20);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(62, 13);
+            this.statusLabel.TabIndex = 7;
+            this.statusLabel.Text = "Scroll: {0,0}";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.scrollNumericUpDown);
+            this.panel1.Controls.Add(this.statusLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.countLabel);
+            this.panel1.Controls.Add(this.saveButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 772);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1156, 51);
+            this.panel1.TabIndex = 8;
+            // 
+            // scrollNumericUpDown
+            // 
+            this.scrollNumericUpDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(11)))));
+            this.scrollNumericUpDown.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.scrollNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.scrollNumericUpDown.ForeColor = System.Drawing.Color.White;
+            this.scrollNumericUpDown.Increment = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.scrollNumericUpDown.Location = new System.Drawing.Point(739, 17);
+            this.scrollNumericUpDown.Name = "scrollNumericUpDown";
+            this.scrollNumericUpDown.Size = new System.Drawing.Size(120, 16);
+            this.scrollNumericUpDown.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(672, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Scroll Step:";
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.Controls.Add(this.startButton);
+            this.headerPanel.Controls.Add(this.goButton);
+            this.headerPanel.Controls.Add(this.headerFillPanel);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Padding = new System.Windows.Forms.Padding(5);
+            this.headerPanel.Size = new System.Drawing.Size(1156, 40);
+            this.headerPanel.TabIndex = 9;
+            // 
+            // headerFillPanel
+            // 
+            this.headerFillPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.headerFillPanel.Controls.Add(this.postUrlTextBox);
+            this.headerFillPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.headerFillPanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.headerFillPanel.Location = new System.Drawing.Point(5, 5);
+            this.headerFillPanel.Name = "headerFillPanel";
+            this.headerFillPanel.Size = new System.Drawing.Size(931, 30);
+            this.headerFillPanel.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(869, 40);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(10, 10, 10, 0);
+            this.panel3.Size = new System.Drawing.Size(287, 732);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.webView21);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 40);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(10, 10, 10, 0);
+            this.panel4.Size = new System.Drawing.Size(869, 732);
+            this.panel4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(11)))), ((int)(((byte)(11)))));
+            this.ClientSize = new System.Drawing.Size(1156, 823);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.headerPanel);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1172, 862);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1172, 862);
+            this.Name = "Form1";
+            this.Text = "X Fav Scraper";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scrollNumericUpDown)).EndInit();
+            this.headerPanel.ResumeLayout(false);
+            this.headerFillPanel.ResumeLayout(false);
+            this.headerFillPanel.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button goButton;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox postUrlTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel headerFillPanel;
+        private System.Windows.Forms.NumericUpDown scrollNumericUpDown;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TwitterFavFetcher/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace TwitterFavFetcher
+{
+    public partial class Form1 : Form
+    {
+        Thread scrapeThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            scrapeThread = new Thread(new ThreadStart(Loop));
+            CheckForIllegalCrossThreadCalls = false;
+            statusLabel.Text = $"ThreadState: {scrapeThread.ThreadState}";
+        }
+
+        void Loop()
+        {
+            while (true)
+            {
+                this.BeginInvoke(new MethodInvoker(delegate
+                {
+                    statusLabel.Text = $"ThreadState: {scrapeThread.ThreadState}";
+                    _ = UpdateListAsync();
+                }));
+                Thread.Sleep(500);
+            }
+        }
+
+        int currentHeight = 0;
+
+        private async Task UpdateListAsync()
+        {
+            string dom = await webView21.ExecuteScriptAsync("document.documentElement.outerHTML");
+
+            dom = Regex.Unescape(dom);
+            dom = dom.Remove(0, 1);
+            dom = dom.Remove(dom.Length - 1, 1);
+
+            var html = new HtmlAgilityPack.HtmlDocument();
+            html.LoadHtml(dom);
+
+
+            HtmlNodeCollection nodes = html.DocumentNode.SelectNodes("//span[contains(@class, 'css-1qaijid') and contains(@class, 'r-bcqeeo')]");
+
+            int count = listBox1.Items.Count;
+
+            foreach (HtmlNode node in nodes)
+                if (node.InnerText.StartsWith("@") && !listBox1.Items.Contains(node.InnerText))
+                    listBox1.Items.Add(node.InnerText);
+
+            countLabel.Text = "Count: " + listBox1.Items.Count;
+
+            currentHeight += (int)scrollNumericUpDown.Value;
+            string s = await webView21.ExecuteScriptAsync("window.scrollTo(0, " + currentHeight + ");");
+
+            int visibleItems = listBox1.ClientSize.Height / listBox1.ItemHeight;
+            listBox1.TopIndex = Math.Max(listBox1.Items.Count - visibleItems + 1, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri(postUrlTextBox.Text);
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            foreach (string i in listBox1.Items)
+                list.Add(i);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files | *.txt";
+            sfd.DefaultExt = "txt";
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+
+            if(sfd.ShowDialog() == DialogResult.OK)
+                File.WriteAllLines(sfd.FileName, list);
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (scrapeThread.ThreadState == ThreadState.WaitSleepJoin || scrapeThread.ThreadState == ThreadState.Running)
+            {
+                scrapeThread.Abort();
+                startButton.Text = "Start";
+                startButton.BackColor = Color.LimeGreen;
+            }
+            else
+            {
+                currentHeight = 0;
+                GC.SuppressFinalize(scrapeThread);
+                scrapeThread = new Thread(new ThreadStart(Loop));
+                scrapeThread.Start();
+                startButton.Text = "Abort";
+                startButton.BackColor = Color.Red;
+            }
+        }
+    }
+}
+//span.css-1qaijid.r-bcqeeo.r-qvutc0.r-poiln3
